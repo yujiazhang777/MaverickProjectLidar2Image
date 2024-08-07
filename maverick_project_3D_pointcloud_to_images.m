56fc4d4fc690dffd80afd98dc1815f3777b6f01f
yujiazhang777 <32459738+yujiazhang777@users.noreply.github.com>
2024-08-07 20:39:43 +0800

    Update matlab code for all 4 sequences of YUTO MMS dataset

--- maverick_project_3D_pointcloud_to_images.m
+++ maverick_project_3D_pointcloud_to_images.m
@@ -4,18 +4,23 @@ clc
 
 %% construct parameters
 
-% boresight angles and lever arm of LiDAR in IMU frame (Sequece A)
-Rs2i=rotxyz([178.869094016 -44.844306361 1.933645172]*pi/180);
-ts2i=[0.111744 0.010389 -0.181398]';
+% boresight angles and lever arm of velodyne LiDAR in IMU frame (from LCP.txt, Sequece A)
+Rv2i=rotxyz([178.869094016 -44.844306361 1.933645172]*pi/180);
+tv2i=[0.111744 0.010389 -0.181398];
+Tv2i=[Rv2i; tv2i];
+% fixed rotation matrix from LiDAR to imu frame
+Rs2v=rotxyz([0 0 -90]*pi/180);
+% Ts2i contains rotation matrix and lever arm from LiDAR to imu frame
+Ts2i=Tv2i*Rs2v;
 
-% boresight angles and lever arm of ladybug camera system in IMU frame (Sequece A)
-Rlbs2i=rotxyz([0.15, -0.05, -179.65]*pi/180);
-Rlb2lbs=rotxyz([0, 0, 180]*pi/180);
-Rlb2i=Rlbs2i*Rlb2lbs;
-tlb2i=[-0.031239, 0, -0.1115382]';
-
-% fixed boresight angles from 
-Rc2lbc=rotxyz([0 0 -90]*pi/180)*rotxyz([180 0 0]*pi/180);
+% boresight angles and lever arm of ladybug camera system in IMU frame (from CCP.txt, Sequece A)
+Rlb2i=rotxyz([0.15, -0.05, -179.65]*pi/180);
+tlb2i=[-0.031239, 0, -0.1115382];
+Tlb2i=[Rlb2i; tlb2i];
+% fixed rotation matrix from the camera frame to imu frame
+Rc2lb=rotxyz([180 0 0]*pi/180);
+% Tc2i contains rotation matrix and lever arm from camera to IMU frame
+Tc2i=Tlb2i*Rc2lb;
 
 % image size
 imSize=[8000, 4000];
@@ -28,7 +33,7 @@ time_L = strrep(filename_L, '.bin', '');
 timeL = cellfun(@str2num,time_L)';
 
 % image sequence timestamp (extracted from groundtruth txt file)
-fid_gt = fopen('../Sequence A/GroundTruth_LBP_HQ_WGS_84_UTM___zone_17N_s.txt');
+fid_gt = fopen('../Sequence D/GroundTruth_LBP_YU_campus_UTM_Zone_17N_r.txt');
 fgetl(fid_gt);
 %File format: ImNum(1), Cam(2), CamImNm(3), Rotate(4), Error(5), GPSTime(6), Index(7), Filename(8), East(9), North(10), Height(11), Omega(12), Phi(13), Kappa(14), img_h(15), img_w(16)
 cell_data_raw = textscan(fid_gt, '%d %d %d %d %d %f %d %s %f %f %f %f %f %f %d %d');     
@@ -51,7 +56,7 @@ Tinterval=(timeL(Tindex)-timeC);
 for i =1:length(timeC)
     
     % load velodyne points from .bin file
-    filename = sprintf('../Sequence A/LidarData/%05d.bin', timeL(Tindex(i)));
+    filename = sprintf('../Sequence D/LidarData/%05d.bin', timeL(Tindex(i)));
     fid_velo = fopen(filename,'rb');
     [points,count]=fread(fid_velo,'float32');
     fclose(fid_velo);
@@ -60,37 +65,31 @@ for i =1:length(timeC)
     velo(2,:) = points(2:4:end);
     velo(3,:) = points(3:4:end);
 
-    xl=rotxyz([0 0 -90]*pi/180)*velo(1:3,:);
-
-    % Transform laser point in sensor frame to imu frame
+    % Transform laser point from sensor frame to imu frame
     clearvars xi
-    xi(1:3,:)=[Rs2i ts2i]*[xl;ones(1,size(velo,2))];
+    xi(1:3,:)=[Ts2i(1:3,1:3) Ts2i(4,1:3)']*[velo;ones(1,size(velo,2))];
      
-%     Project point 3d point in the camera coordinate systme onto the panoramic image:
+    % Project point 3d point in the camera coordinate systme onto the panoramic image:
     panoWidth = imSize(1);
     panoHeight = imSize(2);
-    
-    Rcc2i=Rlb2i*Rc2lbc;
-    tcc2i=tlb2i;
 
-    xc=inv(Rcc2i)*(xi-tcc2i);
+    % Transform laser point from imu frame to camera frame
+    xc=inv(Tc2i(1:3,1:3))*(xi-Tc2i(4,1:3)');
 
-    xcc=[xc(2,:);-xc(1,:);xc(3,:)];
-
-   % The figure visualizes mapping results
+    % The figure visualizes mapping results
     hp=figure(1)
     
-%     bearing vector to theta phi
-    theta = atan2(xcc(2,:),xcc(1,:))/pi*180 + 180;
-    phi = atan2(sqrt(xcc(1,:).^2+xcc(2,:).^2),xcc(3,:))/pi*180;
+    % bearing vector to theta phi
+    theta = atan2(xc(2,:),xc(1,:))/pi*180 + 180;
+    phi = atan2(sqrt(xc(1,:).^2+xc(2,:).^2),xc(3,:))/pi*180;
 
-%     theta phi to image coordinates
+    % theta phi to image coordinates
     up = panoWidth - (theta/360.0)*panoWidth;
     vp = phi/180.0*panoHeight;
     
     % read the panoramic image
     panonames = cell_data_raw{8};
-    panoname = sprintf('../Sequence A/PanoramicImages/%s', strrep(panonames{i,1},'.tif','.jpg'));
+    panoname = sprintf('../Sequence D/PanoramicImages/%s', strrep(panonames{i,1},'.tif','.jpg'));
     Ip = imread(panoname);
     imshow(Ip)
     hold on
@@ -100,7 +99,7 @@ for i =1:length(timeC)
     rowp = size(Ip,1) + 1;
         
     %range of points
-    range_camc = xcc.*xcc;
+    range_camc = xc.*xc;
     rangep = sqrt(sum(range_camc,1));
     [sorted_rangep ii_rp] = sort(rangep);
         
